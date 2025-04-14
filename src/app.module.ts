734c6731b9006e10296f0a8c7e5b293763494b90
Kayk_Darios <manjac688@gmail.com>
2025-04-13 21:35:39 -0300

    chore: importando para a aplicacao os modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProjetosModule } from './projetos/projetos.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProjetosModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
